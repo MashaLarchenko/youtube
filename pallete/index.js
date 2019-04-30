8276444fd259ae35b93843f27ce3fdf89883a606
Masha Larchenko <mashalarchenko16@gmail.com>
2019-04-30 21:42:40 +0300

    feat: implement move tool

--- pallete/index.js
+++ pallete/index.js
@@ -1,54 +1,31 @@
-const colorPicker = document.querySelector('.choose-color');
 
-const paintBucket = document.querySelector(".paint-bucket");
-
-const move = document.querySelector(".move");
-
-const transfotm = document.querySelector(".transform");
 
 const currentColor = document.querySelector('.current-color');
 
-const prevColor = document.querySelector(".prev-color");
+const prevColor = document.querySelector('.prev-color');
 
-const tools = document.querySelector(".tools-container");
+const tools = document.querySelector('.tools-container');
 
-const figureElem = document.querySelector(".figure-wrapper");
+const figureElem = document.querySelector('.figure-wrapper');
 
-// const fugot = document.querySelector('.figure-item');
+const dragEl = document.querySelector('.figure-item');
 
-const customColor = document.querySelector('input[type="color"]').value;
+const customColor = document.getElementById('customColor');
+
 
 const state = {
-    currentTool: ''
+  currentTool: '',
+  currentcolor: currentColor,
+  previColor: prevColor,
 };
 
-tools.addEventListener('click', (event) => {
-    if (event.target.classList.contains('choose-color')) {
-        state.currentTool = 'colorPicker';
-    } else if (event.target.classList.contains('paint-bucket')) {
-        state.currentTool = 'paintBucket';
-    } else if (event.target.classList.contains('move')) {
-        state.currentTool = 'move';
-    } else if (event.target.classList.contains('transform')) {
-        state.currentTool = 'transform';
-    } else {
-        state.currentTool = '';
-    }
-    console.log('click', event);
-    console.log(state.currentTool);
-});
 
-figureElem.addEventListener('click', (event) => {
-    console.log(state.currentTool);
-    if (state.currentTool === 'paintBucket') {
-        let elem = event.target;
-        elem.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
-        console.log(elem.style.backgroundColor);
-    } else if (state.currentTool === 'transform') {
-        changeShape(event);
-    }
+const stor = JSON.stringify(state);
 
-});
+
+localStorage.setItem('state', stor);
+
+const s = localStorage.getItem('state');
 
 function changeShape(event) {
   if (event.target.classList.contains('square')) {
@@ -61,27 +38,79 @@ function changeShape(event) {
 }
 
 
+function key(ev) {
+  if (event.onkeydown = e => e.keyCode === '0x43') {
+    console.log(ev);
+    return true;
+  }
+}
 
-document.addEventListener('click', (event) => {
-    if (state.currentTool === 'colorPicker') {
-        let ccolor = window.getComputedStyle(event.target).backgroundColor;
-        prevColor.style.backgroundColor = currentColor.style.backgroundColor;
-        currentColor.style.backgroundColor = ccolor;
+figureElem.addEventListener('mousedown', (e) => {
+  if (state.currentTool === 'move') {
+    e.target.style.position = 'absolute';
+    moveAt(e);
+    document.body.appendChild(dragEl);
+    e.target.style.zIndex = 20;
+
+    function moveAt(e) {
+      e.target.style.left = `${e.pageX - e.target.offsetWidth / 2}px`;
+      e.target.style.top = `${e.pageY - e.target.offsetHeight / 2}px`;
+      e.target.style.zIndex = 50;
     }
+
+    document.onmousemove = function (e) {
+      moveAt(e);
+    };
+
+    figureElem.onmouseup = function () {
+      document.onmousemove = null;
+      figureElem.onmouseup = null;
+      figureElem.onkeydown = null;
+    };
+
+    figureElem.ondragstart = function () {
+      return false;
+    };
+  }
 });
 
 
+tools.addEventListener('click', (event) => {
+  if (event.target.classList.contains('choose-color')) {
+    state.currentTool = 'colorPicker';
+  } else if (event.target.classList.contains('paint-bucket')) {
+    state.currentTool = 'paintBucket';
+  } else if (event.target.classList.contains('move')) {
+    state.currentTool = 'move';
+  } else if (event.target.classList.contains('transform')) {
+    state.currentTool = 'transform';
+  } else {
+    state.currentTool = '';
+  }
+  // console.log('click', event);
+  // console.log(state.currentTool);
+});
 
 
+figureElem.addEventListener('click', (event) => {
+  if (state.currentTool === 'paintBucket') {
+    const elem = event.target;
+    elem.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+  } else if (state.currentTool === 'transform') {
+    changeShape(event);
+  }
+});
 
 
-// document.addEventListener ('click', (event) => {
-//    if(event.target == colorPicker) {
-//        state.currentTool = 'colorPicker';
-//    } 
-//    if (event.target ===  paintBucket) {
-//     state.currentTool = 'paintBucket';
-//    }
-// });
-
-
+document.addEventListener('click', (event) => {
+  if (state.currentTool === 'colorPicker') {
+    if (event.target === customColor) {
+      const customValue = document.getElementById('customColor').value;
+      state.currentcolor.style.backgroundColor = customValue;
+    } else {
+      const targetColor = window.getComputedStyle(event.target).backgroundColor;
+      state.previColor.style.backgroundColor = state.currentcolor.style.backgroundColor;
+      state.currentcolor.style.backgroundColor = targetColor;
+    }
+  }
+});
