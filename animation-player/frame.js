3cf01009f6269347c8bec56127a9e9ed300d06b0
Masha Larchenko <mashalarchenko16@gmail.com>
2019-06-04 18:40:08 +0300

    feat: add copy

--- animation-player/frame.js
+++ animation-player/frame.js
@@ -1,4 +1,6 @@
 const frame = document.querySelector('.frame');
+const anim = document.querySelector('.prev');
+const context = anim.getContext('2d');
 
 const frames = document.querySelector('.frames');
 // const main = document.querySelector('.main');
@@ -12,14 +14,12 @@ const framesContainer = [frame];
 
 
 addButton.addEventListener('click', (e) => {
-  // console.log(e.target);
-  // const newEL = document.createElement('li');
-  // console.log(frame);
-  // newEL.appendChild(frame);
   count++;
   const newFr = frame.cloneNode(true);
   newFr.className += ` frame${count}`;
   const newButton = newFr.querySelector('.fa-trash-alt');
+  const newCopyButton = newFr.querySelector('.fa-copy');
+  newCopyButton.className += ` frame${count}`;
   newButton.className += ` frame${count}`;
   console.log(newButton);
   frames.appendChild(newFr);
@@ -32,14 +32,37 @@ frames.addEventListener('click', (e) => {
   if (e.target.classList.contains('fa-trash-alt')) {
     const item = e.target.classList[2];
     const r = document.getElementsByClassName(item);
-     frames.removeChild(r[0]);
+    frames.removeChild(r[0]);
   }
 });
 
 
 frames.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-copy')) {
+    const n = e.target.classList[2];
+    const copyEl = document.getElementsByClassName(n);
+    console.log(copyEl);
+    const copy = copyEl[0].cloneNode(true);
+    count++;
+    copy.className += ` frame${count}`;
+    const nButton = copy.querySelector('.fa-trash-alt');
+    nButton.className += ` frame${count}`;
+    frames.appendChild(copy);
+    framesContainer.push(copy);
+  }
+
+
   if (e.target.classList.contains('frame')) {
     e.target.className += ' active';
   }
   console.log(e.target.className);
+});
+
+requestAnimationFrame(() => {
+  framesContainer.forEach((el) => {
+    const pro = el.children[0].toDataURL('image/png');
+    const image = new Image();
+    image.src = pro;
+    context.drawImage(image, 0, 0, 300, 300);
+  });
 });
