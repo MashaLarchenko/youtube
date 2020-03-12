40a5aa524b7c5ad3bf8a5c59da445768846ac345
maria larchenko <mashalarchenko16@gmail.com>
2020-03-12 16:26:43 +0300

    refactor: btn style

--- youtube-client/src/views/AppView/SliderButton.js
+++ youtube-client/src/views/AppView/SliderButton.js
@@ -1,5 +1,5 @@
 export default class SliderButton {
   static render() {
-    return '<div class="slider-button"><button class="prev">Previous</button><button class="current"></button><button class="next">Next</button></div>';
+    return '<div class="slider-button"><button class="sliderbtn prev">Previous</button><button class="sliderbtn current"></button><button class="sliderbtn next">Next</button></div>';
   }
 }
